6413cb18663345a7c06cc50a335cabf3527b30f7
Rammah <rammahkarpous@outlook.com>
2022-09-08 07:41:08 +0200

    feat: add seo component

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'sql496.main-hosting.eu'),
+      host: env('DATABASE_HOST'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'u985382793_strapi'),
-      user: env('DATABASE_USERNAME', 'u985382793_strapiv4'),
-      password: env('DATABASE_PASSWORD', '8k@oxh/#Z'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
