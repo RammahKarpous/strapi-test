71d6a804109ec5dcb640d84f5c267451cb8604a6
Rammah <rammahkarpous@outlook.com>
2022-09-10 15:41:13 +0200

    chore: changed port and ssl env variables

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ module.exports = ({ env }) => ({
     client: 'mysql',
     connection: {
       host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT', 3306),
+      port: 3306,
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true),
+      ssl: true,
     },
   },
 });
